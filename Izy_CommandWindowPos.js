9675527b3b755c6d97ce83eb8f16dcd87eafcacf
Accendor <58440978+Accendor@users.noreply.github.com>
2022-02-23 21:03:48 +0100

    Add files via upload

---
+++ Izy_CommandWindowPos.js
@@ -0,0 +1,76 @@
+//=============================================================================
+// RPG Maker MZ - Command window position (Combat)
+//-----------------------------------------------------------------------------
+// Izy_CommandWindowPos.js
+// This plugin was requested by Accendor. This plugin reposition the combat party and command window in battle.
+//=============================================================================
+
+/*:
+ * @target MZ
+ * @plugindesc This plugin was requested by Accendor. This plugin reposition the combat party and command window in battle.
+ * @author Izyees Fariz
+ * @url https://forums.rpgmakerweb.com/
+ *
+ * @help
+ *
+ * This plugin was requested by Accendor. For any edit/improvement/fix please
+ * contact me at the forum (izyees).
+ * This plugin reposition the coordinate of the command window.
+ * Use the text tab in the plugin manager to input a negative value.
+ * 
+ * This plugin does not provide plugin commands.
+ *
+ * @param WinX
+ * @text Window X coordinate
+ * @desc Set the X coordinate of the battle window. The default value was set to 0. Use text for negative values.
+ * @default 0
+ * @type number
+ *
+ *
+ * @param WinY
+ * @text Window Y coordinate
+ * @desc Set the Y coordinate of the battle window.  The default value was set to 0. Use text for negative values.
+ * @default 0
+ * @type number
+ */
+ 
+ var Izyees = Izyees || {};
+ Izyees.plugins = Izyees.plugins || {};
+ Izyees.plugins.Accendor = {};
+ Izyees.plugins.Accendor.params = PluginManager.parameters('Izy_CommandWindowPos');
+ 
+ (function(){
+	const Params = Izyees.plugins.Accendor.params;
+	
+	//The party command window
+	Scene_Battle.prototype.partyCommandWindowRect = function() {
+		const ww = 192;
+		const wh = this.windowAreaHeight();
+		const wx = Params.WinX;
+		const wy = Params.WinY;
+		return new Rectangle(wx, wy, ww, wh);
+	};
+
+	//The actor command window
+	Scene_Battle.prototype.actorCommandWindowRect = function() {
+		const ww = 192;
+		const wh = this.windowAreaHeight();
+		const wx = Params.WinX;
+		const wy = Params.WinY;
+		return new Rectangle(wx, wy, ww, wh);
+	};
+	
+	//Remove the commandWindow.y callout
+	Scene_Battle.prototype.createActorCommandWindow = function() {
+		const rect = this.actorCommandWindowRect();
+		const commandWindow = new Window_ActorCommand(rect);
+		commandWindow.setHandler("attack", this.commandAttack.bind(this));
+		commandWindow.setHandler("skill", this.commandSkill.bind(this));
+		commandWindow.setHandler("guard", this.commandGuard.bind(this));
+		commandWindow.setHandler("item", this.commandItem.bind(this));
+		commandWindow.setHandler("cancel", this.commandCancel.bind(this));
+		this.addWindow(commandWindow);
+		this._actorCommandWindow = commandWindow;
+	};
+
+ })();
